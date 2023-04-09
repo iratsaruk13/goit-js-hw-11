9e240b5ad242f1ffaa0d5450f2991a4eb0a00dcf
iratsaruk13 <115734138+iratsaruk13@users.noreply.github.com>
2023-04-09 23:22:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,123 +1,135 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';
+import axios from "axios";
+import { Notify } from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import fetchImages from './fetchImages';
-// import createGalleryMarkup from './createGalleryMarkup';
 
-const searchForm = document.querySelector('#search-form')
-const gallery = document.querySelector('.gallery');
-const buttonLoadMore = document.querySelector('.load-more');
+const searchForm = document.querySelector('.search-form');
+const galleryContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more')
 
-let isShown = 0;
-const galleryApi = new GalleryApi();
-let lightbox = new SimpleLightbox('.photo-card a', {
-  captions: true,
+let page = 1;
+let isVisible = 0;
+
+loadMoreBtn.style.display = 'none';
+
+searchForm.addEventListener('submit', onSearchQuery);
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onLoadMore() { 
+  loadMoreBtn.style.display = 'none';
+    page += 1;
+    const name = searchForm.querySelector('input').value.trim();
+    pixabayGallaryAPI(name, page);
+    loadMoreBtn.style.display = 'flex';
+
+}
+
+
+searchForm.querySelector('input');
+function onSearchQuery(evt) { 
+    evt.preventDefault();
+    isVisible = 0;
+    galleryContainer.innerHTML = '';
+
+    const name = searchForm.querySelector('input').value.trim();
+
+    if (name !== '') {
+      pixabayGallaryAPI(name);
+    } else { 
+      loadMoreBtn.style.display = 'none';
+      return Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+        );
+    }
+}
+
+async function pixabayGallaryAPI(name, page) { 
+    const URL = 'https://pixabay.com/api/';
+
+    const options = {
+        params: { 
+            key: '34989371-d98b3f165afd7d2509d6f9eb9',
+            q: name, 
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: 'true',
+            page: page,
+            per_page: 40,
+        },
+    };
+
+    try {
+        const response = await axios.get(URL, options);
+        isVisible += response.data.hits.length;
+
+        message(
+            response.data.hits.length,
+            isVisible,
+            options.params.per_page,
+            response.data.total
+        );
+
+        createGalleryMarkup(response.data);
+    } catch (error) { 
+        console.log(error);
+    }
+}
+
+const simpleLightBox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
-// const options = {
-//   rootMargin: '50px',
-//   root: null,
-//   threshold: 0.3,
-// };
 
-// const observer = new IntersectionObserver(makeLoadMore, options);
-
-
-searchForm.addEventListener('submit', onSearchQuery);
-buttonLoadMore.addEventListener('click', makeLoadMore);
-
-function onSearchQuery(evt) {
-  evt.preventDefault();
-
-  gallery.innerHTML = '';
-  galleryApi.query =
-    evt.currentTarget.elements.searchQuery.value.trim();
-    galleryApi.resetPage();
-
-  if (galleryApi.query === '') {
-    Notify.warning('Please, fill the main field');
-    return;
-  }
-
-  isShown = 0;
-  fetchImages();
-  createGalleryMarkup(hits);
+function createGalleryMarkup(items) { 
+    const renderMarkup = items.hits.map(item =>
+        `<a href="${item.largeImageURL}">
+            <div class="photo-card">
+            <div>
+            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
+            </div>
+                    <div class="card-info">
+                        <p class="card-item">
+                            <b>Likes</b>
+                            ${item.likes}
+                        </p>
+                        <p class="card-item">
+                            <b>Views</b>
+                            ${item.views}
+                        </p>
+                        <p class="card-item">
+                            <b>Comments</b>
+                            ${item.comments}
+                        </p>
+                        <p class="card-item">
+                            <b>Downloads</b>
+                            ${item.downloads}
+                        </p>
+                    </div>
+            </div>
+        </a>`).join('');
+        galleryContainer.insertAdjacentHTML('beforeend', renderMarkup);
+    simpleLightBox.refresh();
 }
 
-function makeLoadMore() {
-  galleryApi.incrementPage();
-  fetchImages();
-}
 
-async function fetchImages() {
-  buttonLoadMore.classList.add('is-hidden');
-
-  const result = await galleryApi.fetchImages();
-  const { hits, total } = result;
-  isShown += hits.length;
-
-  if (!hits.length) {
-   Notiflix.Notify.failure(
-      `Sorry, there are no images matching your search query. Please try again.`
-    );
-    buttonLoadMore.classList.add('is-hidden');
-    return;
-  }
-
-  createGalleryMarkup(hits);
-  isShown += hits.length;
-
-  if (isShown < total) {
-   Notiflix.Notify.success(`Hooray! We found ${total} images !!!`);
-    buttonLoadMore.classList.remove('is-hidden');
-  }
-
-  if (isShown >= total) {
-   Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-  }
-}
-
-function createGalleryMarkup(el) {
-  const markup = el
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<div class="photo-card">
-    <a href="${largeImageURL}">
-      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-    </a>
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-        ${likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-        ${views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-        ${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-        ${downloads}
-      </p>
-    </div>
-    </div>`;
-      }
-    )
-    .join('');
-  gallery.insertAdjacentHTML('beforeend', markup);
-  lightbox.refresh();
+function message(length, isVisible, per_page, total) { 
+    if (!length) { 
+        Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+        );
+    }
+    if (length >= isVisible) { 
+      loadMoreBtn.style.display = 'flex';
+        Notify.info(
+            `Hooray! We found ${total} images.`
+        );
+    }
+    if (isVisible >= total) { 
+        Notify.info(
+            "We're sorry, but you've reached the end of search results."
+        );
+        loadMoreBtn.style.display = 'none';
+    }
 }
